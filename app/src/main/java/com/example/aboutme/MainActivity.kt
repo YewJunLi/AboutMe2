3a5f28f0c555425c00d4a628696fb79785862876
yewjunli21 <yewjunli21@gmail.com>
2021-07-01 17:51:36 +0800

    initial commit

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDone : Button = findViewById(R.id.done_button)
+
+        btnDone.setOnClickListener(){
+
+            val tvInput : TextView = findViewById(R.id.nickname_edit)
+            val tvResult : TextView = findViewById(R.id.tvResult)
+
+            tvResult.text = tvInput.text
+            tvResult.visibility = View.VISIBLE
+            btnDone.visibility = View.GONE
+            tvInput.visibility = View.GONE
+        }
+
     }
 }
